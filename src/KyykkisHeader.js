8b156c131fa8d57d89b50b58bcab371eeb64ec07
Viljami Anttonen <viljami.anttonen@gmail.com>
2017-06-13 00:57:38 +0300

    Tuned logo 🥄

--- src/KyykkisHeader.js
+++ src/KyykkisHeader.js
@@ -1,12 +1,13 @@
 import './KyykkisHeader.css'
 
 import React from 'react'
+import chromatism from 'chromatism'
 
 export default () => (
   <header className="KyykkisHeader">
     <span>Kyykkis Hotline</span>
-    <span>Kyykkis Hotline</span>
-    <span>Kyykkis Hotline</span>
-    <span>Kyykkis Hotline</span>
+    <span style={{ color: chromatism.brightness(-25, '#ff0044').hex }}>Kyykkis Hotline</span>
+    <span style={{ color: chromatism.brightness(-35, '#ff0044').hex }}>Kyykkis Hotline</span>
+    <span style={{ color: chromatism.brightness(-40, '#ff0044').hex }}>Kyykkis Hotline</span>
   </header>
 )
